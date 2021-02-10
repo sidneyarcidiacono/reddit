d591852f720244ec0d45fd7f7889d4ce8bb4d166
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-02-10 15:36:48 -0800

    Included User model methods to encrypt and decrypt passwords.

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose")
+const bcrypt = require('bcrypt')
+const salt = process.env.SECRET
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
@@ -9,5 +11,26 @@ const UserSchema = new Schema({
 },
   {timestamps: {createdAt: 'created_at'}}
 )
+
+UserSchema.pre("save", function (next) {
+    // ENCRYPT PASSWORD
+    const user = this
+    if (!user.isModified("password")) {
+        return next()
+    }
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            user.password = hash
+            next()
+        })
+    })
+})
+
+// Need to use function to enable this.password to work.
+UserSchema.methods.comparePassword = function (password, done) {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+        done(err, isMatch)
+    })
+}
 
 module.exports = mongoose.model("User", UserSchema)
