bbab53227b3ccaf4ea9b3535725c77cc7d64df8c
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-02-17 18:33:16 -0800

    Implemented initial authentication tests.

--- test/posts.js
+++ test/posts.js
@@ -9,6 +9,9 @@ const server = require('../server')
 chai.should()
 chai.use(chaiHttp)
 
+// Agent that will keep track of our cookies
+const agent = chai.request.agent(server)
+
 it('Should create with valid attributes at POST /posts/new', function(done) {
   // Checks how many posts there are now
   Post.estimatedDocumentCount()
@@ -19,7 +22,14 @@ it('Should create with valid attributes at POST /posts/new', function(done) {
             // since we're not actually filling out a form
             .set("content-type", "application/x-www-form-urlencoded")
             // Make a request to create another
-            .send(newPost)
+            .send({
+              title: "TestPost",
+              url: "http://localhost:3000",
+              summary: "A test post",
+              subreddit: "n/new",
+              comments: [],
+              author: "aaaaaaaaaaaa"
+            })
             .then(function (res) {
                 Post.estimatedDocumentCount()
                     .then(function (newDocCount) {
@@ -43,5 +53,5 @@ it('Should create with valid attributes at POST /posts/new', function(done) {
 })
 
 after(function () {
-  Post.findOneAndDelete(newPost);
+  Post.findOneAndDelete({title: "TestPost"});
 })
